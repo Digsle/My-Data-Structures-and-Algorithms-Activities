36e2086e2ab5e07f684ab93ca58293b5633d9073
Digsle <hyrdib@gmail.com>
2024-10-10 21:20:14 +0800

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Palindrome {
+	public static boolean Palindrome(String word) {
+		word = word.toLowerCase();
+		String reversed = new StringBuilder (word).reverse().toString();
+		return word.equals(reversed);
+		}
+		public static void main(String [] args){
+			String [] words = {"madam", "atoyota", "rabar", "pogi"};
+
+			for (String word : words){
+				if (Palindrome(word)) {
+					System.out.println( "  " +word+ " is a Palindrome");
+				}
+				else {
+					System.out.println( "  " +word+ " is not a Palindrome");
+				}
+		}
+
+	}
+}
